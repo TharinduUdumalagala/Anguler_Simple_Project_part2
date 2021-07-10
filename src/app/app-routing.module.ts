dbb1d68bb99967160a3e9275efc244eb411801ec
TharinduUdumalagala <tharindugamage0414@gmail.com>
2021-07-10 13:22:47 +0100

    Anguler_part2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerPageComponent} from "./components/customer-page/customer-page.component";
+import {ItemPageComponent} from "./components/item-page/item-page.component";
+import {OdrerPageComponent} from "./components/order-page/odrer-page.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
+import {OrderGuard} from "./guards/order.guard";
+import {LoginPageComponent} from "./components/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component:CustomerPageComponent},
+  {path:'',redirectTo:'/customer',pathMatch:'full'},
+  {path:'customer', component:CustomerPageComponent},
+  {path:'item', component:ItemPageComponent},
+  {path:'order', component:OdrerPageComponent,canActivate:[OrderGuard]},
+  {path:'login', component:LoginPageComponent},
+  {path:'**', component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/order-page/odrer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-odrer-page',
+  templateUrl: './odrer-page.component.html',
+  styleUrls: ['./odrer-page.component.scss']
+})
+export class OdrerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
