1cef834d5d22aaf20e88ba92ce93e60eb1390c82
TharinduUdumalagala <tharindugamage0414@gmail.com>
2021-07-10 18:34:45 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,21 @@ import {OdrerPageComponent} from "./components/order-page/odrer-page.component";
 import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 import {OrderGuard} from "./guards/order.guard";
 import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {SaveCustomerPageComponent} from "./components/customer-page/items/save-customer-page/save-customer-page.component";
+import {UpdateCustomerPageComponent} from "./components/customer-page/items/update-customer-page/update-customer-page.component";
+import {DeleteCustomerPageComponent} from "./components/customer-page/items/delete-customer-page/delete-customer-page.component";
+import {SearchCustomerPageComponent} from "./components/customer-page/items/search-customer-page/search-customer-page.component";
 
 const routes: Routes = [
   // {path:'',component:CustomerPageComponent},
   {path:'',redirectTo:'/customer',pathMatch:'full'},
-  {path:'customer', component:CustomerPageComponent},
+  {path:'customer', component:CustomerPageComponent, children:[
+      {path:'',redirectTo:'/customer/save-customer',pathMatch:'full'},
+      {path:'save-customer', component: SaveCustomerPageComponent},
+      {path:'update-customer', component: UpdateCustomerPageComponent},
+      {path:'delete-customer', component: DeleteCustomerPageComponent},
+      {path:'search-customer', component: SearchCustomerPageComponent}
+    ]},
   {path:'item', component:ItemPageComponent},
   {path:'order', component:OdrerPageComponent,canActivate:[OrderGuard]},
   {path:'login', component:LoginPageComponent},
